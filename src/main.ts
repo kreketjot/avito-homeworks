9ce2b1cf43dd92aa6d524ed86dd34444405d3114
Andrey Vinokurov <kreketjot@gmail.com>
2023-03-04 21:30:28 +0300

    .env PORT

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { VersioningType } from '@nestjs/common';
 
+const PORT = process.env.PORT;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableVersioning({
     type: VersioningType.URI,
     defaultVersion: '1',
   });
-  await app.listen(3000);
+  await app.listen(PORT);
 }
 bootstrap();
